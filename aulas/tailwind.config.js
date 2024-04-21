a04b573fb54f046671c952ff635f7756a2fdf444
Macedo Francisco <macedolb53@gmail.com>
2024-04-21 13:09:59 +0200

    darkMode

--- aulas/tailwind.config.js
+++ aulas/tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*{html,js}"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        brand: '#fccc'
+      },
+      fontFamily:{
+        sans: ['Roboto', 'sans-serif'],
+        serif: ['Jersey 25','serif']
+      },
+      // backgroundImage:{
+      //   banner: "url('/img/banner.jpg')"
+      // }
+    },
   },
   plugins: [],
 }
