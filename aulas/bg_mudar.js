30479b045e7d7b6ee0baee78f47adf6ec086c407
Francisco Macedo <macedolb53@gmail.com>
2024-04-24 22:22:36 +0200

    imagem

--- aulas/bg_mudar.js
+++ aulas/bg_mudar.js
@@ -3,6 +3,15 @@ const hora =tempo.getHours()
 const bg_mudar = document.querySelector("#bg-mudar");
 const titulo = document.querySelector("#titulo");
 const imagembg = document.createElement("img");
+const imagem_exbir = document.querySelector("#imagem-exbir")
+const btnLeft =document.querySelector("#esquerdo")
+const btnRight = document.querySelector("#direito") 
+
+let imagemActual =0
+const imagens =[
+    "url(../photo/quarto.jpg)",
+    "url(../photo/pisicina.jpg)"
+];
 
 imagembg.setAttribute("src", " url(./photo/bg-second.jpg)")
 
@@ -15,5 +24,16 @@ if(hora > 18){
 
 }
 
+btnLeft.addEventListener('click', () => {
+    imagem_exbir.style.backgroundImage = "url(../aulas/photo/varanda.jpg)"
+
+    if(imagemActual < imagens.length){
+        imagem_exbir.style.backgroundImage += "url(../aulas/photo/quarto.jpg)"
+    }
+  
+})
+btnRight.addEventListener('click',()=>{
+    imagem_exbir.style.backgroundImage = "url(../aulas/photo/camas.jpg)"
+})
 
 console.log("A gora sao "+hora);
